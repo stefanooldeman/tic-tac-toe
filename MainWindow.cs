af13089ddc0fca6e6afd423b8d52482bb15a3573
stefanooldeman <stefano.oldeman@gmail.com>
2012-09-20 13:20:30 +0200

    update w/ licence information, free for all

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,3 +1,13 @@
+
+/*
+ * This file is part of the tictactoe package.
+ *
+ * (c) Stefano Oldeman <stefano.oldeman@gmail.com>
+ *
+ * For the full copyright and license information, please view the README.md
+ * file that was distributed with this source code.
+ */
+
 using System;
 using Gtk;
 using System.Collections.Generic;
